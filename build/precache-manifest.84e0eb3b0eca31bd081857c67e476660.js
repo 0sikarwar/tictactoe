c4f8a3346044fd1bc920657f466466a310bd127f
Gyanender Singh <gyanendersingh5@gmail.com>
2021-09-12 11:19:51 +0530

    added gh-pages

---
+++ build/precache-manifest.84e0eb3b0eca31bd081857c67e476660.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c61cb58f8d77191b96d5",
+    "url": "/tictactoe/static/css/main.4b6dc839.chunk.css"
+  },
+  {
+    "revision": "c61cb58f8d77191b96d5",
+    "url": "/tictactoe/static/js/main.c61cb58f.chunk.js"
+  },
+  {
+    "revision": "35ca0c160d1f5a426caf",
+    "url": "/tictactoe/static/js/1.35ca0c16.chunk.js"
+  },
+  {
+    "revision": "a6a71d35c1fbb3241cd8",
+    "url": "/tictactoe/static/js/runtime~main.a6a71d35.js"
+  },
+  {
+    "revision": "4ff8f2dce84476336896e4ae5171994a",
+    "url": "/tictactoe/index.html"
+  }
+];
